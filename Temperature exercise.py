0c7080169d93824047f7bd63ae6ee5ef427eae00
40904@longroad.ac.uk <40904@PC004152.coll.lr.local>
2014-09-30 12:56:13 +0100

    Temperature exercise

---
+++ Temperature exercise.py
@@ -0,0 +1,14 @@
+#Kavilan Naidoo
+#30-09-2014
+#Temperature exercise
+
+temperature= float(input("Please enter the temperature of the water in the container:"))
+
+if temperature >=100:
+    print("The water is boiling")
+elif temperature <=0:
+    print(" The water is frozen")
+else:
+    print("The water is neither boiling or frozen")
+    
+    
