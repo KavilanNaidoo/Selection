46ba0c787a1febe49cc2c382559a529fb922182e
40904@longroad.ac.uk <40904@PC004152.coll.lr.local>
2014-09-30 12:07:01 +0100

    selection improvement exercise

--- if_improvement_exercise.py
+++ if_improvement_exercise.py
@@ -3,30 +3,30 @@
 #26-09-2014
 
 month = int(input("Please enter a month as a number between 1-12: "))
-if month == 1:
-    print("The month you entered is January")
-elif month == 2:
-    print("The month you entered is February")
-elif month == 3:
-    print("The month you entered is March")
-elif month == 4:
-    print("The month you entered is April")
-elif month == 5:
-    print("The month you entered is May")
-elif month == 6:
-    print("The month you entered is June")
-elif month == 7:
-    print("The month you entered is July")
-elif month == 8:
-    print("The month you entered is August")
-elif month == 9:
-    print("The month you entered is September")
+if month == 12:
+    print("The month you entered is December")
+elif month ==11 :
+    print("The month you entered is November")
 elif month == 10:
     print("The month you entered is October")
-elif month == 11:
-    print("The month you entered is November")
-elif month == 12:
-    print("The month you entered is December")
+elif month == 9:
+    print("The month you entered is September")
+elif month ==8:
+    print("The month you entered is August")
+elif month == 7:
+    print("The month you entered is July")
+elif month == 6:
+    print("The month you entered is June")
+elif month ==5 :
+    print("The month you entered is May")
+elif month == 4:
+    print("The month you entered is April")
+elif month == 3:
+    print("The month you entered is March ")
+elif month == 2:
+    print("The month you entered is February")
+elif month == 1:
+    print("The month you entered is January")
 else:
     print("You have entered an invalid number - try again")
     
