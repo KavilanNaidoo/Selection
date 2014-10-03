3050e3f6ded39517b871d30e075ab0ba4fb4e2e1
40904@longroad.ac.uk <40904@PC004152.coll.lr.local>
2014-10-03 14:03:34 +0100

    Grade boundaries

---
+++ Grade boundaries.py
@@ -0,0 +1,19 @@
+#Kavilan Naidoo
+#02-10-2014
+#Grade boundaries
+
+
+score=int(input("Please enter your score: "))
+if score > 80:
+    print("You got an A-grade")
+elif score > 70:
+    print("You got a B-grade")
+elif score > 60:
+    print("You got a C-grade")
+elif score > 50:
+    print("You got a D-grade")
+elif score > 40:
+    print("You got an E-grade")
+else:
+    print("You got a U-grade")
+    
